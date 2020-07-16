db858e4febef021ba3ead5b44eaae4502b2559a9
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-17 01:18:22 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,6 +1,7 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::collections::HashMap;
 #[fastout]
 fn main() {
     input! {
@@ -9,12 +10,19 @@ fn main() {
         m: usize,
         t: [usize; m],
     }
+    let mut map_d: HashMap<usize, usize> = HashMap::new();
+    for i in 0..n {
+        let counter = map_d.entry(d[i]).or_insert(0);
+        *counter += 1;
+    }
     for i in 0..m {
-        if !d.contains(&t[i]) {
-            println!("No");
-            return;
+        let counter = map_d.entry(t[i]).or_insert(0);
+        if *counter > 0 {
+            *counter -= 1;
         } else {
+            println!("NO");
+            return;
         }
     }
-    println!("Yes");
+    println!("YES");
 }
